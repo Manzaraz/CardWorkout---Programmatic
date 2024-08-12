01dc2f954f68ec8d55964d4f8b2d35b1fb1344df
Christian Manzaraz <magnusmanz@gmail.com>
2024-08-12 17:55:12 -0300

    Programmatic - App Logic

--- CardWorkout - Programmatic/CardSelectionVC.swift
+++ CardWorkout - Programmatic/CardSelectionVC.swift
@@ -14,13 +14,45 @@ class CardSelectionVC: UIViewController {
     let resetButton     = CWButton(backgroundColor: .systemGreen, title: "Reset")
     let rulesButton     = CWButton(backgroundColor: .systemBlue, title: "Rules")
     
+    var cards: [UIImage] = CardDeck.allValues
+    var timer: Timer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
+        
         configureUI()
+        startTimer()
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        stopTimer()
+    }
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.1,
+                                     target: self,
+                                     selector: #selector(showRandomCard),
+                                     userInfo: nil,
+                                     repeats: true)
+    }
+    
+    @objc func stopTimer() {
+        timer.invalidate()
+    }
+    
+    @objc func resetTimer() {
+        stopTimer()
+        startTimer()
+    }
+    
+    
+    @objc func showRandomCard () {
+        cardImageView.image = cards.randomElement()
+    }
+    
     
     func configureUI() {
         configureCardImageView()
@@ -46,6 +78,7 @@ class CardSelectionVC: UIViewController {
     
     func configureStopButton() {
         view.addSubview(stopButton)
+        stopButton.addTarget(self, action: #selector(stopTimer), for: .touchUpInside)
         
         NSLayoutConstraint.activate([
             stopButton.widthAnchor.constraint(equalToConstant: 260),
@@ -59,6 +92,8 @@ class CardSelectionVC: UIViewController {
     
     func configureResetButton()  {
         view.addSubview(resetButton)
+        resetButton.addTarget(self, action: #selector(resetTimer), for: .touchUpInside)
+        
         NSLayoutConstraint.activate([
             resetButton.widthAnchor.constraint(equalToConstant: 115),
             resetButton.heightAnchor.constraint(equalToConstant: 50),
